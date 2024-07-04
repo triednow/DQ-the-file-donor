fc85112551398507bc9d9ca862ba10fa0d0fb19b
admin449 <158821779+admin449@users.noreply.github.com>
2024-07-04 12:12:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
-echo "Cloning Custom Repo from"
-git clone https://github.com/admin449/DQTheFileDonorBot
-cd DQTheFileDonorBot
-pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
